00d46a049c0c2140763008f6ba587fe981e76e0a
Lyota <korystin.aleksei@yandex.ru>
2023-04-30 20:42:02 +0300

    add update Fav, Like, Black list

--- Vk_bot/Vk_bot_class.py
+++ Vk_bot/Vk_bot_class.py
@@ -6,9 +6,10 @@ from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 import sys
 import time
 
+"""Открытие токина для бота"""
+
 
 def open_a_token(file_name):
-    """Открытие токина для бота"""
     config = ConfigParser()
     config.read(file_name)
     group_token = config["Vk_info"]["group_token"]
@@ -16,14 +17,18 @@ def open_a_token(file_name):
 
 
 class VkBot:
+
+    """Подключение к боту"""
+
     def __init__(self, open_a_token):
-        """Подключение к боту"""
         self.vk_session = vk_api.VkApi(token=open_a_token)
         self.vk_api = self.vk_session.get_api()
         self.long_pool = VkLongPoll(self.vk_session)
+        self.get_parametrs = {}
+
+    """Сообщение от бота: кому, сообщение, id собщения, кнопки, картинки"""
 
     def write_msg(self, user_id, message, keyboard=None, attachment=None):
-        """Сообщение от бота: кому, сообщение, id собщения, кнопки, картинки"""
         post = {
             "user_id": user_id,
             "message": message,
@@ -39,8 +44,9 @@ class VkBot:
             post = post
         self.vk_session.method("messages.send", post)
 
+    """Список цветов"""
+
     def but_col(self):
-        """Список цветов"""
         buttons_colors = [
             VkKeyboardColor.POSITIVE,
             VkKeyboardColor.NEGATIVE,
@@ -49,8 +55,9 @@ class VkBot:
         ]
         return buttons_colors
 
+    """Задаём цвет, количество кнопок в ответе бота"""
+
     def set_key_parameters(self, buttons, but_col):
-        """Задаём цвет, количество кнопок в ответе бота"""
         keyboard = VkKeyboard(one_time=True)
         if not isinstance(buttons, list) and not isinstance(but_col, list):
             buttons = [buttons]
@@ -65,8 +72,9 @@ class VkBot:
             count += 1
         return keyboard
 
+    """Собщения боту от пользователя"""
+
     def get_message(self):
-        """Собщения боту от пользователя"""
         for event in self.long_pool.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                 message = event.text.lower()
@@ -77,6 +85,40 @@ class VkBot:
         """Приветствие"""
         response = "Добро пожаловать в бот Vkinder.\nВыберите команду!"
         return response
+
+    """Кнопки в приветствии бота, главное меню"""
+
+    def greetings_bot(self):
+        buttons = [
+            "Избранные",
+            "Мне нравится",
+            "Черный список",
+            "Начать поиск",
+            "Завершить работу с ботом",
+        ]
+        but_col = self.but_col()
+        keyboard = self.set_key_parameters(
+            buttons, [but_col[2], but_col[2], but_col[2], but_col[0], but_col[1]]
+        )
+        return keyboard
+
+    """Сообщение боту"""
+
+    def query_bot(self):
+        message, self.user_id = self.get_message()
+        return message, self.user_id
+
+    """Показать гавлное меню"""
+
+    def show_main_menu(self):
+        # msg, user_id = self.query_bot()
+        keyboard = self.greetings_bot()
+        self.write_msg(self.user_id, self.command_list_output(), keyboard)
+
+    """Стартовое меню"""
+
+    def start_bot(self):
+        self.show_main_menu()
 
     """Работа со списком избранных"""
 
@@ -246,44 +288,10 @@ class VkBot:
                     self.start_bot()
                     break
 
-    """Кнопки в приветствии бота, главное меню"""
-
-    def greetings_bot(self):
-        buttons = [
-            "Избранные",
-            "Мне нравится",
-            "Черный список",
-            "Начать поиск",
-            "Завершить работу с ботом",
-        ]
-        but_col = self.but_col()
-        keyboard = self.set_key_parameters(
-            buttons, [but_col[2], but_col[2], but_col[2], but_col[0], but_col[1]]
-        )
-        return keyboard
-
-    """Сообщение боту"""
-
-    def query_bot(self):
-        message, self.user_id = self.get_message()
-        return message, self.user_id
-
-    """Показать гавлное меню"""
-
-    def show_main_menu(self):
-        # msg, user_id = self.query_bot()
-        keyboard = self.greetings_bot()
-        self.write_msg(self.user_id, self.command_list_output(), keyboard)
-
-    """Стартовое меню"""
-
-    def start_bot(self):
-        self.show_main_menu()
-
     """Показ сообщения "Напишите возраст" и Возрат к выбору пола"""
 
     def back_to_gender(self, user_id):
-        msg = "Напишите возраст особи"
+        msg = "Напишите возраст особи цифрами, например 18."
         buttons = "Назад, к выбору пола."
         but_col = self.but_col()
         keyboard = self.set_key_parameters(buttons, but_col[1])
@@ -298,6 +306,62 @@ class VkBot:
         keyboard = self.set_key_parameters(buttons, but_col[1])
         self.write_msg(user_id, msg, keyboard)
 
+    """Обработка анкет, добавить в избранные, мне нравится или черный список"""
+
+    def add_to_list(self, user_id):
+        def the_end(user_id):
+            if count >= len(result_requests_vkontakte) - 1:
+                buttons = "Вернуться в главное меню"
+                but_col = self.but_col()
+                keyboard = self.set_key_parameters(buttons, but_col[1])
+                self.write_msg(user_id, "Это была последняя анкета", keyboard)
+                message, user_id = self.get_message()
+                if message.lower() == "вернуться в главное меню":
+                    self.start_bot()
+
+        result_requests_vkontakte = []  # Тут вставить, что возращает Вк после обработки, список либо словарь.
+        if result_requests_vkontakte == []:
+            msg = "По вашему запросу анкет нет, измените параметры запроса!"
+            but_col = self.but_col()
+            keyboard = self.set_key_parameters("Вернуться в главное меню", but_col[1])
+            self.write_msg(user_id, msg, keyboard)
+            message, user_id = self.query_bot()
+            if message == "вернуться в главное меню":
+                self.start_bot()
+        elif result_requests_vkontakte != []:
+            for count, info_user in enumerate(result_requests_vkontakte):
+                self.write_msg(
+                    user_id,
+                    f"{info_user}",  # Вставить фотографии {user.first_name}, {user.last_name}, 'Ссылка',
+                    keyboard=None,
+                    attachment=None,
+                )
+                buttons = [
+                    "Добавить в избранное",
+                    "Добавить в мне нравится",
+                    "Добавить в черный список",
+                    "Дальше",
+                    "Вернуться в главное меню",
+                ]
+                but_col = self.but_col()
+                keyboard = self.set_key_parameters(
+                    buttons,
+                    [but_col[2], but_col[2], but_col[2], but_col[0], but_col[0]],
+                )
+                self.write_msg(user_id, "Выберите действие", keyboard)
+                message, user_id = self.get_message()
+                if message.lower() == "добавить в избранное":
+                    """Тут вставить функцию БД на добавление в избранное, информации храниться в info_user"""
+                elif message.lower() == "добавить в мне нравится":
+                    """Тут вставить функцию БД на добавление в мне нравится, информации храниться в info_user"""
+                elif message.lower() == "добавить в черный список":
+                    """Тут вставить функцию БД на добавление в черный список, информации храниться в info_user"""
+                elif message.lower() == "дальше":
+                    the_end(user_id)
+                elif message.lower() == "вернуться в главное меню":
+                    self.start_bot()
+                    break
+
     """Опрос пользователя для составления словаря для поиска."""
 
     def start_search(self, user_id):
@@ -306,23 +370,25 @@ class VkBot:
             if message.lower() == "назад, к выбору пола.":
                 self.start_search(user_id)
             elif int(message) >= 18:
-                get_parametrs["age"] = message
+                self.get_parametrs["age"] = message
                 self.back_to_age(user_id)
                 message, user_id = self.query_bot()
                 if message.lower() == "назад, к выбору возраста.":
                     self.back_to_gender(user_id)
                     search_age_city()
                 else:
-                    get_parametrs["city"] = message
+                    self.get_parametrs["city"] = message
                     msg = "Данные записаны, начинаем поиск!"
                     self.write_msg(user_id, msg, keyboard=None)
+                    # time.sleep(3)
+                    # self.add_to_list(user_id)
             elif int(message) < 18:
                 msg = "Аккуратно! Статься 134 УК РФ!"
                 self.write_msg(user_id, msg, keyboard=None)
                 time.sleep(5)
                 self.start_search(user_id)
 
-        get_parametrs = {}
+        self.get_parametrs = {}
         msg = "Выберите пол особи"
         buttons = [
             "Особь женского пола",
@@ -337,13 +403,13 @@ class VkBot:
         message, user_id = self.query_bot()
 
         if message.lower() == "особь женского пола":
-            get_parametrs[
+            self.get_parametrs[
                 "sex"
             ] = "1"  # ВОТ ТУТ НЕ ЗНАЮ В ОТВЕТЕ ОТ ВКОНТАКТЕ ЧТО ПЕРЕДАЕТСЯ INT ИЛИ STR
             self.back_to_gender(user_id)
             search_age_city()
         elif message.lower() == "особь мужского пола":
-            get_parametrs[
+            self.get_parametrs[
                 "sex"
             ] = "2"  # ВОТ ТУТ НЕ ЗНАЮ В ОТВЕТЕ ОТ ВКОНТАКТЕ ЧТО ПЕРЕДАЕТСЯ INT ИЛИ STR
             self.back_to_gender(user_id)
@@ -351,7 +417,6 @@ class VkBot:
         elif message == "вернуться в главное меню":
             self.start_bot()
             main()
-        return get_parametrs
 
 
 def main():
@@ -369,7 +434,10 @@ def main():
     elif message.lower() == "черный список":
         vk_session.show_black_list(user_id)
     elif message.lower() == "начать поиск":
-        res = vk_session.start_search(user_id)  # переменная для поиска
+        vk_session.start_search(user_id)
+        vk_session.get_parametrs  # Для поиска по Вконтакте
+        time.sleep(3)
+        vk_session.add_to_list(user_id)
     elif message.lower() == "завершить работу с ботом":
         msg = "До свидания"
         vk_session.write_msg(user_id, msg, keyboard=None)
